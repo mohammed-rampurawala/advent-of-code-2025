1a696d9f9af93a98230270d638efb7e393c24aed
Mohammed R <mzeus.bolt@gmail.com>
2025-12-01 09:15:41 +0000

    Simplified the usage of mod

--- days/src/main/day01.kt
+++ days/src/main/day01.kt
@@ -1,6 +1,7 @@
 package main
 
 import java.io.File
+import java.lang.Math.floorMod
 
 fun main() {
     val input = File("inputs/private/day01.txt").readText().trimEnd()
@@ -17,8 +18,8 @@ fun solvePart1(lines: List<String>): Any {
         val dir = line[0]
         val dist = line.substring(1).toInt()
         position = when (dir) {
-            'L' -> ((position - dist) % 100 + 100) % 100
-            'R' -> (position + dist) % 100
+            'L' -> floorMod(position - dist, 100)
+            'R' -> floorMod(position + dist,100)
             else -> position
         }
         if (position == 0) zeroCount++
@@ -36,26 +37,15 @@ fun solvePart2(lines: List<String>): Any {
         val dir = line[0]
         val dist = line.substring(1).toInt()
 
-        val occurrences = when (dir) {
-            'R' -> {
-                var firstK = (100 - (position % 100)) % 100
-                if (firstK == 0) firstK = 100
-                if (firstK > dist) 0 else 1 + (dist - firstK) / 100
-            }
-            'L' -> {
-                var firstK = position % 100
-                if (firstK == 0) firstK = 100
-                if (firstK > dist) 0 else 1 + (dist - firstK) / 100
-            }
+        val step = when(dir){
+            'L' -> -1
+            'R' -> 1
             else -> 0
         }
 
-        zeroCount += occurrences
-
-        position = when (dir) {
-            'L' -> ((position - dist) % 100 + 100) % 100
-            'R' -> (position + dist) % 100
-            else -> position
+        for (i in 1..dist) {
+            position = floorMod(position + step, 100)
+            if (position == 0) zeroCount++
         }
     }
 
